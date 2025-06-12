29967acbc06a732758608f1288b6032ceff5c6da
RM Muzammil <ramuzamil44@gmail.com>
2025-06-12 14:40:37 +0500

    temperary public products route

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const productController = require("../controller/product.controller.js");
 const authenticate = require("../middleware/authenticate.js");
 
-router.get("/", authenticate, productController.getAllProducts);
-router.get("/id/:id", authenticate, productController.findProductById);
+// router.get("/", authenticate, productController.getAllProducts);
+// router.get("/id/:id", authenticate, productController.findProductById);
+router.get("/", productController.getAllProducts);
+router.get("/id/:id", productController.findProductById);
 
 module.exports = router;
