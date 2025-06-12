f022ea981d31853d5be639cf2bbf8cc9b86ff4ac
RM Muzammil <ramuzamil44@gmail.com>
2025-06-12 14:50:44 +0500

    testing_7

---
+++ temp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+function Navbar() {
+  return (
+    <div>
+      <nav className="buttom">
+        <Link to="/Home">Home</Link>
+        <Link to="/Dashboard">Dashboard</Link>
+        <Link to="/Login">Login</Link>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
